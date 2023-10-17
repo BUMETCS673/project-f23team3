e5b13e4c7394520d9e654f27951061c21e1968c8
songzhuoru <songzhuoru@gmail.com>
2023-10-17 11:38:14 +0800

    Create cart page

--- code/app.py
+++ code/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request
 import flask_sqlalchemy as alchemy
 from LoginPage import login_layout
 from RegiserPage import register_layout
@@ -8,15 +8,16 @@ from ConfirmPage import confirm_layout
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///testDB.db'
 app.register_blueprint(login_layout)
 app.register_blueprint(register_layout)
 app.register_blueprint(menu_layout)
 app.register_blueprint(cart_layout)
 app.register_blueprint(confirm_layout)
 
-@app.route("/")
-def hello():
-    return "Home Page!"
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
