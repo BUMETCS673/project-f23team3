ba98c1d29890469ebc5f7fb123b1755398459246
Quin <145232577+QuinNiu@users.noreply.github.com>
2023-10-28 23:55:45 -0400

    1

--- code/ConfirmPage.py
+++ code/ConfirmPage.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from app import db
+from Models import db
 
 confirm_layout = Blueprint('confirm_layout', __name__)
 
