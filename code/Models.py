b74420084c0d5f926087b186ac8fa91d7e750146
chandrasiris <chsa@bu.edu>
2023-11-09 14:02:03 -0500

    Add buttons to add to cart. Database and models restructure required before merge.

--- code/Models.py
+++ code/Models.py
@@ -3,41 +3,51 @@ from app import db
 Base = db.Model
 
 
-class Order(Base):
+class Orders(Base):
     __tablename__ = 'orders'
     id = db.Column(db.Integer, primary_key=True)
+    table_id = db.Column(db.Integer, db.ForeignKey('dining_tables.id'))
     customer_id = db.Column(db.Text, db.ForeignKey('customers.id'))
     date = db.Column(db.Text)
     status = db.Column(db.Text)
-    total = db.Column(db.Integer)
-    server_id = db.Column(db.Text, db.ForeignKey('staffs.id'))
+    payment = db.Column(db.Integer)
+    request = db.Column(db.Text)
 
 
-class Party(Base):
-    __tablename__ = 'parties'
-    table_id = db.Column(db.Integer, db.ForeignKey('dining_tables.id'), primary_key=True)
-    customer_id = db.Column(db.Text, db.ForeignKey('customers.id'), primary_key=True)
+class Ingredients(Base):
+    __tablename__ = 'ingredients'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text)
+    cost = db.Column(db.REAL)
+    expiration_date = db.Column(db.Text)
+    quantity_left = db.Column(db.REAL)
 
 
-class Cart(Base):
-    __tablename__ = 'carts'
-    table_id = db.Column(db.Integer, db.ForeignKey('dining_tables.id'), primary_key=True)
-    dish_id = db.Column(db.Integer, db.ForeignKey('dishes.id'), primary_key=True)
-    quantity = db.Column(db.Integer)
-    special = db.Column(db.Text)
+class Reviews(Base):
+    __tablename__ = 'reviews'
+    id = db.Column(db.Integer, primary_key=True)
+    dish_id = db.Column(db.Integer, db.ForeignKey('Order.id'))
+    customer_id = db.Column(db.Text, db.ForeignKey('Customer.id'))
+    feedback = db.Column(db.Text)
 
 
-class Customer(Base):
+class Customers(Base):
     __tablename__ = 'customers'
     id = db.Column(db.Text, primary_key=True)
     name = db.Column(db.Text)
     email = db.Column(db.Text)
 
 
-class Dish(Base):
+class Compositions(Base):
+    __tablename__ = 'compositions'
+    dish_id = db.Column(db.Integer, db.ForeignKey('dishes.id'), primary_key=True)
+    ingredient_id = db.Column(db.Integer, db.ForeignKey('ingredients.id'), primary_key=True)
+
+
+class Dishes(Base):
     __tablename__ = 'dishes'
     id = db.Column(db.Integer, primary_key=True)
-    general_dish_id = db.Column(db.Integer, primary_key=True)
+    main_dish_id = db.Column(db.Integer)
     name = db.Column(db.Text)
     description = db.Column(db.Text)
     cost = db.Column(db.REAL)
@@ -45,8 +55,8 @@ class Dish(Base):
     num_left = db.Column(db.Integer)
 
 
-class DishType(Base):
-    __tablename__ = 'dish_types'
+class General_dishes(Base):
+    __tablename__ = 'general_dishes'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.Text)
     description = db.Column(db.Text)
@@ -58,20 +68,28 @@ class Requests(Base):
     order_id = db.Column(db.Integer, db.ForeignKey('orders.id'), primary_key=True)
     dish_id = db.Column(db.Integer, db.ForeignKey('dishes.id'), primary_key=True)
     quantity = db.Column(db.Integer)
+    special_requests = db.Column(db.Text)
+
+
+class Cart(Base):
+    __tablename__ = 'cart'
+    user_id = db.Column(db.Integer, db.ForeignKey('customers.id'), primary_key=True)
+    dish_id = db.Column(db.Integer, db.ForeignKey('dishes.id'), primary_key=True)
+    quantity = db.Column(db.Integer)
     special = db.Column(db.Text)
 
 
-class DiningTable(Base):
+class DiningTables(Base):
     __tablename__ = 'dining_tables'
     id = db.Column(db.Integer, primary_key=True)
-    server = db.Column(db.Integer, db.ForeignKey('staffs.id'))
+    server = db.Column(db.Integer, db.ForeignKey('staff.id'))
     status = db.Column(db.Text)
-    passphrase = db.Column(db.Text)
+    capacity = db.Column(db.Integer)
 
 
 class Staff(Base):
-    __tablename__ = 'staffs'
-    id = db.Column(db.Text, primary_key=True)
+    __tablename__ = 'staff'
+    id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.Text)
     last_name = db.Column(db.Text)
     email = db.Column(db.Text)
