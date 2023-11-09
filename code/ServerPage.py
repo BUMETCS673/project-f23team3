3c80bb329bc99a5a83121808a6ee36c5771e4e78
Quin <145232577+QuinNiu@users.noreply.github.com>
2023-11-09 03:07:39 -0500

    add the showing completed orders history function

--- code/ServerPage.py
+++ code/ServerPage.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, render_template, request, jsonify, session, redirect
-from DataOP import *
+from flask import Blueprint, render_template, session, redirect
 from sqlalchemy import event
+from Models import *
+from DataOP import *
 
 server_layout = Blueprint('server_layout', __name__)
 
@@ -33,3 +34,10 @@ def server():
     else:
         # Not worker or not on duty (include manager and kitchen), return to order page.
         return render_template('staff.html', name=find_name_from_id(user_id))
+
+
+@server_layout.route('/completed-orders')
+def completed_orders():
+    completed_orders = Orders.query.filter_by(status='completed').all()
+    return render_template('completed_orders.html', orders=completed_orders)
+# list all the completed orders
