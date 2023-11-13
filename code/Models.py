8ac633374c2582442b7f9aa0ea73bbec32366bd4
Tony <qtx2023@bu.edu>
2023-11-13 01:59:20 -0500

    Conflicts resolved, Models.py structure and main.db are up-to-date as **11/13/2023**

--- code/Models.py
+++ code/Models.py
@@ -3,24 +3,28 @@ from app import db
 Base = db.Model
 
 
-class Orders(Base):
+class Order(Base):
     __tablename__ = 'orders'
     id = db.Column(db.Integer, primary_key=True)
     table_id = db.Column(db.Integer, db.ForeignKey('dining_tables.id'))
-    customer_id = db.Column(db.Text, db.ForeignKey('customers.id'))
     date = db.Column(db.Text)
     status = db.Column(db.Text)
     payment = db.Column(db.Integer)
     request = db.Column(db.Text)
 
 
-class Ingredients(Base):
-    __tablename__ = 'ingredients'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.Text)
-    cost = db.Column(db.REAL)
-    expiration_date = db.Column(db.Text)
-    quantity_left = db.Column(db.REAL)
+class Party(Base):
+    __tablename__ = 'parties'
+    order_id = db.Column(db.Integer, db.ForeignKey('orders.id'), primary_key=True)
+    customer_id = db.Column(db.Text, db.ForeignKey('customers.id'), primary_key=True)
+
+
+class Cart(Base):
+    __tablename__ = 'carts'
+    table_id = db.Column(db.Integer, db.ForeignKey('dining_tables.id'), primary_key=True)
+    dish_id = db.Column(db.Integer, db.ForeignKey('dishes.id'), primary_key=True)
+    quantity = db.Column(db.Integer)
+    special = db.Column(db.Text)
 
 
 class Reviews(Base):
@@ -31,23 +35,23 @@ class Reviews(Base):
     feedback = db.Column(db.Text)
 
 
-class Customers(Base):
+class Customer(Base):
     __tablename__ = 'customers'
     id = db.Column(db.Text, primary_key=True)
     name = db.Column(db.Text)
     email = db.Column(db.Text)
 
 
-class Compositions(Base):
+class Composition(Base):
     __tablename__ = 'compositions'
     dish_id = db.Column(db.Integer, db.ForeignKey('dishes.id'), primary_key=True)
     ingredient_id = db.Column(db.Integer, db.ForeignKey('ingredients.id'), primary_key=True)
 
 
-class Dishes(Base):
+class Dish(Base):
     __tablename__ = 'dishes'
     id = db.Column(db.Integer, primary_key=True)
-    main_dish_id = db.Column(db.Integer, primary_key=True)
+    general_dish_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.Text)
     description = db.Column(db.Text)
     cost = db.Column(db.REAL)
@@ -55,8 +59,8 @@ class Dishes(Base):
     num_left = db.Column(db.Integer)
 
 
-class General_dishes(Base):
-    __tablename__ = 'general_dishes'
+class DishType(Base):
+    __tablename__ = 'dish_types'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.Text)
     description = db.Column(db.Text)
@@ -71,15 +75,7 @@ class Requests(Base):
     special_requests = db.Column(db.Text)
 
 
-class Cart(Base):
-    __tablename__ = 'cart'
-    user_id = db.Column(db.Integer, db.ForeignKey('customers.id'), primary_key=True)
-    dish_id = db.Column(db.Integer, db.ForeignKey('dishes.id'), primary_key=True)
-    quantity = db.Column(db.Integer)
-    special = db.Column(db.Text)
-
-
-class DiningTables(Base):
+class DiningTable(Base):
     __tablename__ = 'dining_tables'
     id = db.Column(db.Integer, primary_key=True)
     server = db.Column(db.Integer, db.ForeignKey('staff.id'))
@@ -87,8 +83,17 @@ class DiningTables(Base):
     capacity = db.Column(db.Integer)
 
 
+class Ingredient(Base):
+    __tablename__ = 'ingredients'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text)
+    cost = db.Column(db.REAL)
+    expiration_date = db.Column(db.Text)
+    quantity_left = db.Column(db.REAL)
+
+
 class Staff(Base):
-    __tablename__ = 'staff'
+    __tablename__ = 'staffs'
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.Text)
     last_name = db.Column(db.Text)
