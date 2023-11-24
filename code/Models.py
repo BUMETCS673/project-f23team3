8b87d412b5d194217aaac1f3ab0358b385901bab
songzhuoru <songzhuoru@gmail.com>
2023-11-25 00:49:53 +0800

    start a new branch

--- code/Models.py
+++ code/Models.py
@@ -1,13 +1,13 @@
-from flask_sqlalchemy import SQLAlchemy
+from app import db
 
-db = SQLAlchemy()
 Base = db.Model
 
 
 class Orders(Base):
+    __tablename__ = 'orders'
     id = db.Column(db.Integer, primary_key=True)
-    table_id = db.Column(db.Text, db.ForeignKey('DiningTable.id'))
-    customer_id = db.Column(db.Text, db.ForeignKey('Customer.id'))
+    table_id = db.Column(db.Integer, db.ForeignKey('dining_tables.id'))
+    customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'))
     date = db.Column(db.Text)
     status = db.Column(db.Text)
     payment = db.Column(db.Integer)
@@ -15,70 +15,81 @@ class Orders(Base):
 
 
 class Ingredients(Base):
+    __tablename__ = 'ingredients'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.Text)
-    cost = db.Column(db.REAL)
+    cost = db.Column(db.Float)
     expiration_date = db.Column(db.Text)
-    quantity_left = db.Column(db.REAL)
+    quantity_left = db.Column(db.Float)
 
 
 class Reviews(Base):
+    __tablename__ = 'reviews'
     id = db.Column(db.Integer, primary_key=True)
-    dish_id = db.Column(db.Integer, db.ForeignKey('Order.id'))
-    customer_id = db.Column(db.Text, db.ForeignKey('Customer.id'))
+    order_id = db.Column(db.Integer, db.ForeignKey('orders.id'))
+    customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'))
     feedback = db.Column(db.Text)
 
 
 class Customers(Base):
-    id = db.Column(db.Text, primary_key=True)
+    __tablename__ = 'customers'
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.Text)
     email = db.Column(db.Text)
 
 
 class Compositions(Base):
-    dish_id = db.Column(db.Integer, db.ForeignKey('Dishes.id'), primary_key=True)
-    ingredient_id = db.Column(db.Integer, db.ForeignKey('Ingredient.id'), primary_key=True)
+    __tablename__ = 'compositions'
+    dish_id = db.Column(db.Integer, db.ForeignKey('dishes.id'), primary_key=True)
+    ingredient_id = db.Column(db.Integer, db.ForeignKey('ingredients.id'), primary_key=True)
 
 
 class Dish_Label(Base):
-    dish_id = db.Column(db.Integer, db.ForeignKey('Dishes.id'), primary_key=True)
+    __tablename__ = 'dish_label'
+    dish_id = db.Column(db.Integer, db.ForeignKey('dishes.id'), primary_key=True)
     label = db.Column(db.Text, primary_key=True)
 
 
 class Dishes(Base):
+    __tablename__ = 'dishes'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.Text)
     description = db.Column(db.Text)
-    cost = db.Column(db.REAL)
+    cost = db.Column(db.Float)
     pic_url = db.Column(db.Text)
     num_left = db.Column(db.Integer)
 
 
 class Requests(Base):
-    order_id = db.Column(db.Integer, db.ForeignKey('Order.id'), primary_key=True)
-    dish_id = db.Column(db.Integer, db.ForeignKey('Dishes.id'), primary_key=True)
+    __tablename__ = 'requests'
+    order_id = db.Column(db.Integer, db.ForeignKey('orders.id'), primary_key=True)
+    dish_id = db.Column(db.Integer, db.ForeignKey('dishes.id'), primary_key=True)
     quantity = db.Column(db.Integer)
     special_requests = db.Column(db.Text)
 
 
 class Cart(Base):
-    user_id = db.Column(db.Text, db.ForeignKey('Customers.id'), primary_key=True)
-    dish_id = db.Column(db.Integer, db.ForeignKey('Dishes.id'), primary_key=True)
+    __tablename__ = 'cart'
+    user_id = db.Column(db.Integer, db.ForeignKey('customers.id'), primary_key=True)
+    dish_id = db.Column(db.Integer, db.ForeignKey('dishes.id'), primary_key=True)
     quantity = db.Column(db.Integer)
     special = db.Column(db.Text)
 
 
-class Dining_tables(Base):
+class DiningTables(Base):
+    __tablename__ = 'dining_tables'
     id = db.Column(db.Integer, primary_key=True)
-    server = db.Column(db.Text, db.ForeignKey('Staff.id'))
+    server = db.Column(db.Integer, db.ForeignKey('staff.id'))
     status = db.Column(db.Text)
     capacity = db.Column(db.Integer)
 
 
-class Staffs(Base):
-    id = db.Column(db.Text, primary_key=True)
+class Staff(Base):
+    __tablename__ = 'staff'
+    id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.Text)
     last_name = db.Column(db.Text)
     email = db.Column(db.Text)
     role = db.Column(db.Text)
     status = db.Column(db.Text)
+
--- code/app.py
+++ code/app.py
@@ -1,31 +1,21 @@
-from flask import Flask, render_template, request
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from Models import db
-from LoginPage import login_layout
-from RegiserPage import register_layout
-from FullMenu import full_menu
-from MainMenu import main_menu
-from CartPage import cart_layout
-from ConfirmPage import confirm_layout
+from datetime import timedelta
+import os
+#import test
 
 
 app = Flask(__name__)
+# Generate a random secret key
+app.config['SECRET_KEY'] = os.urandom(24)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///main.db'
-db.init_app(app)
-app.register_blueprint(login_layout)
-app.register_blueprint(register_layout)
-app.register_blueprint(full_menu)
-app.register_blueprint(cart_layout)
-app.register_blueprint(confirm_layout)
-app.register_blueprint(main_menu)
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(hours=1)
+db = SQLAlchemy(app)
+with app.app_context():
+    db.create_all()
+    #test.TestCartFunctionality()
+app.config.from_object(__name__)
 
+# Do not edit the below import, it is by design.
+import views
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-
-if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
