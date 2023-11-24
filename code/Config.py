79c1f4c08882fcfe75c1bb131e3ef45d45fcb8c0
songzhuoru <53034319+songzhuoru@users.noreply.github.com>
2023-11-25 01:07:40 +0800

    Delete code/Config.py

--- code/Config.py
+++
@@ -1,6 +0,0 @@
-firebase_config = {
-    "apiKey": "AIzaSyDoH3S1IVUUTbN-Mh5c5xl5cgORZWEzzJQ",
-    "authDomain": "better-menu-fall23-bumet.firebaseapp.com",
-    "databaseURL": "https://better-menu-fall23-bumet-default-rtdb.firebaseio.com",
-    "storageBucket": "better-menu-fall23-bumet.appspot.com",
-}
