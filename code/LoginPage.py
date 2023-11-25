87b85f07a40581bc6469eeb98d3ef5875a7ac502
Siyuan Xue <xuesy@bu.edu>
2023-11-24 19:08:54 -0500

    update UI design and add login judgment logic (hide cart when not logged in)

--- code/LoginPage.py
+++ code/LoginPage.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, Flask, render_template, url_for, request
+from flask import Blueprint, Flask, render_template, url_for, request, redirect, session
 import firesecure
 
 login_layout = Blueprint('login_api', __name__)
@@ -12,10 +12,15 @@ def login_page():
         password = request.form.get("inputPassword")
         # Actual Registration are handled in local file firesecure.py
         try:
+            # firesecure.login_with_email 处理登录逻辑并返回用户对象
             user = firesecure.login_with_email(email, password)
-            return render_template("login.html", success=True)
+            session['logged_in'] = True  # 设置会话变量标记用户已登录
+            return redirect(url_for('main_menu.main_page'))  # 重定向到菜单页面
         except ValueError as err:
             return render_template("login.html", success=False, error=err)
     else:
         # Handle initial GET request to render the registration page
         return render_template("login.html")
+
+
+# 我直接返回到home不行么？
--- code/app.py
+++ code/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
+
 from flask_sqlalchemy import SQLAlchemy
 from Models import db
 from LoginPage import login_layout
@@ -24,6 +25,11 @@ app.register_blueprint(main_menu)
 def index():
     return render_template('index.html')
 
+# isLoggedIn变量，前端脚本会使用这个变量来显示或隐藏购物车链接
+@app.route('/')
+def home_page():
+    is_logged_in = 'user_id' in session  # session来跟踪登录状态
+    return render_template('home.html', isLoggedIn=is_logged_in)
 
 if __name__ == "__main__":
     with app.app_context():
