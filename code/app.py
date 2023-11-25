00c3c308fd40efd5ca7fd93aabff3209cd06f346
Siyuan Xue <xuesy@bu.edu>
2023-11-24 19:08:54 -0500

    update UI design and add login judgment logic (hide cart when not logged in)

--- code/app.py
+++ code/app.py
@@ -1,21 +1,16 @@
 from flask import Flask, render_template, request, session
+
 from flask_sqlalchemy import SQLAlchemy
 from Models import db
-from datetime import timedelta
 from LoginPage import login_layout
 from RegiserPage import register_layout
 from FullMenu import full_menu
 from MainMenu import main_menu
 from CartPage import cart_layout
 from ConfirmPage import confirm_layout
-import os
 
-
-secret_key = os.urandom(24)
 
 app = Flask(__name__)
-app.secret_key = secret_key
-app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=60)  # set session lifetime to 30 minutes
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///main.db'
 db.init_app(app)
 app.register_blueprint(login_layout)
@@ -30,6 +25,11 @@ app.register_blueprint(main_menu)
 def index():
     return render_template('index.html')
 
+# isLoggedIn变量，前端脚本会使用这个变量来显示或隐藏购物车链接
+@app.route('/')
+def home_page():
+    is_logged_in = 'user_id' in session  # session来跟踪登录状态
+    return render_template('home.html', isLoggedIn=is_logged_in)
 
 if __name__ == "__main__":
     with app.app_context():
