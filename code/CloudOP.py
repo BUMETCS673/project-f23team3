c425d2bdf2a3d2dab8bb63b52b1c37c52e9bc148
Tony <qtx2023@bu.edu>
2023-12-03 18:37:21 -0500

    Populated Menu Pictures and descriptions.

--- code/CloudOP.py
+++ code/CloudOP.py
@@ -1,38 +1,36 @@
-import pyrebaselite as pyrebase
 import requests
-import json
 from Config import firebase_config as config
-
-# Initialize firebase
-firebase = pyrebase.initialize_app(config)
-auth = firebase.auth()
-storage = firebase.storage()
-
-
-def get_menu_pic(file_name):
-    url = storage.child("menu_pics/{}".format(file_name)).get_url(token=None)
-    return url
 
 
 def register_with_email(email, password):
-    try:
-        user = auth.create_user_with_email_and_password(email, password)
-        return user
-    except requests.exceptions.HTTPError as e:
-        error_json = e.args[1]
-        error = json.loads(error_json)['error']['message']
-        raise ValueError(error) from None
-
-
-def login_with_email(email, password):
-    try:
-        user = auth.sign_in_with_email_and_password(email, password)
-        return user
-    except requests.exceptions.HTTPError as e:
-        error_json = e.args[1]
-        error = json.loads(error_json)['error']['message']
+    url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + config["apiKey"]
+    data = {
+        'email': email,
+        'password': password,
+        'returnSecureToken': True
+    }
+    response = requests.post(url, data=data)
+    if response.ok:
+        return response.json()
+    else:
+        # Handle error if response is not ok
+        error = response.json()['error']['message']
         print(error)
         raise ValueError(error) from None
 
 
-print(get_menu_pic('Hummingbird.jpg'))
+def login_with_email(email, password):
+    url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' + config["apiKey"]
+    data = {
+        'email': email,
+        'password': password,
+        'returnSecureToken': True
+    }
+    response = requests.post(url, data=data)
+    if response.ok:
+        return response.json()
+    else:
+        # Handle error if response is not ok
+        error = response.json()['error']['message']
+        print(error)
+        raise ValueError(error) from None
