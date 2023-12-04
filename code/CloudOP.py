823f76252d75414f85796ddd7a15ab6798142da2
Tony <qtx2023@bu.edu>
2023-12-03 20:34:17 -0500

    Role based navigation bar.

--- code/CloudOP.py
+++ code/CloudOP.py
@@ -1,9 +1,7 @@
 import requests
 from Config import firebase_config as config
 
-
-def register_with_email(email, password):
-    url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + config["apiKey"]
+def firebase_request(url, email, password):
     data = {
         'email': email,
         'password': password,
@@ -17,20 +15,13 @@ def register_with_email(email, password):
         error = response.json()['error']['message']
         print(error)
         raise ValueError(error) from None
+
+
+def register_with_email(email, password):
+    url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + config["apiKey"]
+    return firebase_request(url, email, password)
 
 
 def login_with_email(email, password):
     url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' + config["apiKey"]
-    data = {
-        'email': email,
-        'password': password,
-        'returnSecureToken': True
-    }
-    response = requests.post(url, data=data)
-    if response.ok:
-        return response.json()
-    else:
-        # Handle error if response is not ok
-        error = response.json()['error']['message']
-        print(error)
-        raise ValueError(error) from None
+    return firebase_request(url, email, password)
