3de76245aae5ab8a1e3887f265f291602e3114d2
Johnnylee105 <144377293+Johnnylee105@users.noreply.github.com>
2023-12-07 15:07:35 -0500

    Update testcartpage.py

--- test/testcartpage.py
+++ test/testcartpage.py
@@ -1,40 +1,39 @@
 import unittest
-from app import app  # 假设 CartPage.py 包含了 Flask 应用实例、数据库实例和 Cart 模型
+from app import app  
 from Models import db, Cart, Dishes
 
 
 class FlaskCartTestCase(unittest.TestCase):
 
     def setUp(self):
-        # 设置测试环境
+        # Setting up the test environment
         app.config['TESTING'] = True
         app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
         self.app = app.test_client()
 
-        # 推送应用上下文
+        # Push application context
         self.app_context = app.app_context()
         self.app_context.push()
 
         db.create_all()
 
     def tearDown(self):
-        # 清理数据库
+        # Clean the database
         db.session.remove()
         db.drop_all()
-        # 不要忘记在测试结束时删除应用上下文
+        # Remove the application context at the end of the test
         self.app_context.pop()
 
     def test_get_cart(self):
-        # 测试 GET 请求
+        # Test GET request
         response = self.app.get('/cart')
         self.assertEqual(response.status_code, 200)
-        # 可以添加更多的断言来检查返回的内容
 
     def test_post_cart(self):
-        # 测试 POST 请求
+        # Test POST request
         response = self.app.post('/cart', data={'dish_name': 'Pizza', 'quantity': '2'})
         self.assertEqual(response.status_code, 200)
-        # 检查数据库是否有新的购物车条目
+        # Check the database for new shopping cart entries
         cart_item = Cart.query.filter_by(dish_id='Pizza').first()
         self.assertIsNotNone(cart_item)
         self.assertEqual(cart_item.quantity, 2)
